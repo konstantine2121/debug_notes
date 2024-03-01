ea00daa27370fb62cb705c89f5f6c93fd0cb2560
konstantine2121 <kach89kach89@gmail.com>
2024-03-01 14:33:23 +0300

    Add src

---
+++ src/DebugNotesExamples/Example_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int times = 10;
+
+            for (int i = 0; i > times; i++)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
