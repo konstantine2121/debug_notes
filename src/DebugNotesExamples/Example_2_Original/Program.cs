17b2aefab7d42b972034f916fda3b3f4c6deccac
konstantine2121 <kach89kach89@gmail.com>
2024-03-17 22:19:08 +0300

    Add Example_2_Original

---
+++ src/DebugNotesExamples/Example_2_Original/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Example_2_Original
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2, j = 1, z = 0, ogri = 0, ij = 1;
+
+            while (j < 10)
+            {
+                while (i < ogri)
+                {
+                    z = i * j;
+                    Console.Write("{0} x {1}= {2}\t", i, j, z);
+                    ++i;
+
+                    ++ij;
+                }
+
+                if (ij < 33)
+                {
+                    ogri = 6;
+                    Console.WriteLine();
+                    j++;
+                    i = 2;
+                }
+                else
+                {
+                    ogri = 10;
+                    Console.WriteLine();
+                    j++;
+                    i = 6;
+                    if (ij == 33)
+                    {
+                        Console.WriteLine();
+                        j = 2;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
