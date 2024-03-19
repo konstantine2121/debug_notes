846dfb0c567ee25415d507878846be2691f82ade
konstantine2121 <kach89kach89@gmail.com>
2024-03-19 03:25:35 +0300

    vault backup: 2024-03-19 03:25:35

--- src/DebugNotesExamples/Example_2_Debug/Program.cs
+++ src/DebugNotesExamples/Example_2_Debug/Program.cs
@@ -5,15 +5,17 @@ namespace Example_2_Debug
 
     internal class Program
     {
-        private const int firstGroupLimit = 6;
 
         static void Main(string[] args)
         {
+            const int firstGroupLimit = 6;
+            const int groupLimit = 10;
             const int multiplierStartValue = 2;
+            const int secondGroupStartValue = firstGroupLimit;
 
-            int firstMultiplier = 2, secondMultiplier = 1, firstMultiplierLimit = 0, outputCounter = 1;
+            int firstMultiplier = 2, secondMultiplier = 1, firstMultiplierLimit = 0, outputCounter = 0;
             ;
-            while (secondMultiplier < 10)
+            while (secondMultiplier < groupLimit)
             {
                 while (firstMultiplier < firstMultiplierLimit)
                 {
@@ -25,7 +27,7 @@ namespace Example_2_Debug
                     ++outputCounter;
                 }
 
-                if (outputCounter < 33)
+                if (outputCounter < 32)
                 {
                     firstMultiplierLimit = firstGroupLimit;
                     Console.WriteLine();
@@ -34,14 +36,14 @@ namespace Example_2_Debug
                 }
                 else
                 {
-                    firstMultiplierLimit = 10;
+                    firstMultiplierLimit = groupLimit;
                     Console.WriteLine();
                     secondMultiplier++;
-                    firstMultiplier = 6;
-                    if (outputCounter == 33)
+                    firstMultiplier = secondGroupStartValue;
+                    if (outputCounter == 32)
                     {
                         Console.WriteLine();
-                        secondMultiplier = 2;
+                        secondMultiplier = multiplierStartValue;
                     }
                 }
             }
